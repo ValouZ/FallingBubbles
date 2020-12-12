89f92e36faf82f2cf0c6ca4b7e4f868959eac1f2
Valentin <57949313+ValouZ@users.noreply.github.com>
2020-12-12 02:51:16 +0100

    Changed cursor as a bubble
    * Change language / setting up class Bubble
    
    * change var to let
    
    * Nothing working aaaaaah
    
    * Cursor is a bubble - All working
    
    * agreement issue

--- script.js
+++ script.js
@@ -1,47 +1,100 @@
 const bubbleContainer = document.getElementById("app-bubble-container");
+let circle = document.querySelector(".circle");
+// On randomize le nombre de bulles sur la page
+let numberOfBubbles = getRandomInt(10, 21);
 
-function Bubble(){
-    this.diameter = getRandomInt(25,150) * 2;
-    this.red = getRandomInt(0, 256);
-    this.green = getRandomInt(0, 256);
-    this.blue = getRandomInt(0, 256);
-    this.color = "rgb(" + this.red + "," + this.green +"," + this.blue + ")";
+// La classe Bubble va permettre d'instancier des bulles avec des nouvelles propriétés à chaque fois
+class Bubble {
+    constructor(diameter, red, blue, green) {
+        this.diameter = diameter;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.color = "rgb(" + red + "," + green + "," + blue + ")";
+    }
 }  
 
-// Ajout de l'évènement à la page
+// On crée une bulle à l'initialisation pour que la bulle 
+// qui sert de curseur ai les bonnes proriétés
+let bubble = createBubble();
+
+// Ajout des évènements à bubbleContainer
+bubbleContainer.addEventListener("mousemove", (event) => { moveCircle(event, bubble) });
 bubbleContainer.addEventListener("click", addBubble);
 
-// On ajoute une nouvelle bulle à la page
-function addBubble(e){
-    let bubble = new Bubble();
-    let x = e.clientX - (bubble.diameter / 2);
-    let y = e.clientY - (bubble.diameter / 2);
-    console.log("X - " + x + " / Y - " + y);
+// Fonction permettant la création d'une nouvelle bulle totalement aléatoire
+function createBubble(){
+    let diameter = getRandomInt(25, 150) * 2;
+    let red = getRandomInt(0, 256);
+    let green = getRandomInt(0, 256);
+    let blue = getRandomInt(0, 256);
+    return (new Bubble(diameter, red, blue, green));
+}
+
+// Fonction permet de déplacer notre bulle curseur en lui attribuant les bonnes couleur
+function moveCircle(event, bubble) {
+    // On attribue les propriétés css suivante à la div ayant pour classe circle
+	TweenLite.to(circle, 0, {
+        css: {
+            width: bubble.diameter,
+            height: bubble.diameter,
+            background: bubble.color, 
+            // Position de la bulle horizontalement
+            left: event.clientX - (bubble.diameter / 2),
+            // Position de la bulle verticalement
+            top: event.clientY - (bubble.diameter / 2)
+        }
+    });
+}
+
+// On ajoute une nouvelle bulle à bubbleContainer
+function addBubble(event){
+    // On récupère sa position en X et en Y qu'on centre
+    let x = event.clientX - (bubble.diameter / 2);
+    let y = event.clientY - (bubble.diameter / 2);
+
+    // Création d'une nouvelle div qui permettra la création d'une bulle
     let displayedBubble = document.createElement("div");
+
+    // Ajout de toutes les spécificités de la bulle par rapport aux données 
+    // aléatoires de l'objet bubble
     displayedBubble.classList.add("bubble");
     displayedBubble.style.backgroundColor = bubble.color;
     displayedBubble.style.width = bubble.diameter + "px";
     displayedBubble.style.height = bubble.diameter + "px";
     displayedBubble.style.left = x + "px";
     displayedBubble.style.top = y + "px";
+
+    // Ajout de la nouvelle bulle à bubbleContainer
     bubbleContainer.appendChild(displayedBubble);
-    makeBubblesFall(10);
+    // Réinitialisation de la bulle avec de nouvelles propriétés
+    bubble = createBubble();
+    // On appelle moveCircle ici uniquement pour actualiser l'affichage du curseur
+    moveCircle(event, bubble);
+    // Au bout de x (parametre) bulles, on les faits tomber
+    makeBubblesFall(numberOfBubbles);
 }
 
-// Fait tomber les bulles si il y en a un nombre = au nombre choisit
-function makeBubblesFall(numberOfBubbles){
+// Fait tomber les bulles si il y en a un nombre = au nombre passé en paramètre
+function makeBubblesFall(nB){
+    // On récupère l'ensemble des bulles de bubbleContainer
     let bubbles = document.getElementsByClassName("bubble");
-    if (bubbles.length == numberOfBubbles){
+    if (bubbles.length == nB){
+        // On réinitialise le nombre de bulles affichables à l'écran
+        numberOfBubbles = getRandomInt(10, 21);
+        // Ajout de la classe fall à chaque bulles
         for(let i = 0; i < bubbles.length; i++){
             bubbles[i].classList.add("fall");
         }
-        bubbleContainer.removeEventListener("click", addBubble);
+        // On empêche l'utilisateur de rajouter des bulles en même temps que l'animation se lance
+        bubbleContainer.removeEventListener("click", addBubble);    
+        // On supprime une à une les bulles de bubbleContainer et on remet l'écoute d'ajout de bulles à la fin du timer
         setTimeout( () => {
             while (bubbles.length > 0){
                 bubbleContainer.removeChild(bubbles[bubbles.length - 1]);
             }
             bubbleContainer.addEventListener("click", addBubble);
-        }, 2200);
+        }, 2000);
     }
 }
 
